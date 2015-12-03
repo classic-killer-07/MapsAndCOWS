ee4022cb04e23382c3e9ccb37c57a01be8fc2965
classic-killer-07 <jaketa99@gmail.com>
2015-12-03 13:01:43 -0600

    We added a drawable
    NEWS ALERT: Hundreds of cows multiple kilometers high have been seen
    destroying entire cities--
    --WARNING: Floods of cows are excaping onto the streets!--
    --BEWARE! Cownados have been seen in arizona crushing entire
    skyscrapers!

--- app/src/main/java/com/example/s2108867/mapsandcows/MapsActivity.java
+++ app/src/main/java/com/example/s2108867/mapsandcows/MapsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -60,6 +61,36 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.cow2))
+        );
+        mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(20, 10))
+                        .title("Marker")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.cow2))
+        );
+        mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(20, -10))
+                        .title("Marker")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.cow2))
+
+        );
+        mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(20, 0))
+                        .title("Marker")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.cow2))
+        );
+        mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10, 5))
+                        .title("Marker")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.cow2))
+        );
+        mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10, -5))
+                        .title("Marker")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.cow2))
+        );
     }
 }
